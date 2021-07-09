d3f28c602f4db558d9f1df209e4c07906222244d
Xianshun Chen <xs0040@gmail.com>
2021-07-09 15:03:14 -0700

    Add sample code for checking correctness

--- cachematrix.R
+++ cachematrix.R
@@ -33,3 +33,62 @@ cacheSolve <- function(x, ...) {
   x$setInverse(im)
   im
 }
+
+## Sample code for checking correcting of the methods
+
+verifyCacheSolve <- function() {
+  set.seed(31)
+  x <- matrix(rnorm(16, mean=0, sd=1), nrow=4, ncol=4)
+  print(x)
+  m <- makeCacheMatrix(x)
+  im <- cacheSolve(m)
+  print(im)
+  im2 <- cacheSolve(m)
+  print(im2)
+  dot_product <- x %*% im
+  print(dot_product)
+  result <- round(dot_product)
+  print(result)
+  if(identical(result, diag(4))) {
+    print('correctly return identity matrix when dot product')
+  } else {
+    print('wrong! dot product result should be a identity matrix')
+  }
+  if(identical(im, im2)) {
+    print('same inverse matrix returned')
+  } else {
+    print('this should not happen')
+  }
+}
+
+## test run in console
+# > source('cachematrix.R')
+# > verifyCacheSolve()
+# [,1]       [,2]       [,3]       [,4]
+# [1,]  0.05557024  1.5062669 -1.2744714  0.1713988
+# [2,] -0.18423859 -0.4447236 -0.7308096 -1.0471108
+# [3,]  1.59576183  0.3903673 -1.0689675 -0.9904124
+# [4,]  0.96483592  0.9191971 -0.3362180 -0.9175380
+# [,1]       [,2]       [,3]       [,4]
+# [1,] -0.2232754 -0.5252438  0.6811167 -0.1775044
+# [2,]  0.2903755 -0.1517368 -0.4753844  0.7405490
+# [3,] -0.4228039 -0.2830118 -0.4763955  0.7582298
+# [4,]  0.2110457 -0.6006252  0.4145513 -0.8124814
+# getting cached data
+# [,1]       [,2]       [,3]       [,4]
+# [1,] -0.2232754 -0.5252438  0.6811167 -0.1775044
+# [2,]  0.2903755 -0.1517368 -0.4753844  0.7405490
+# [3,] -0.4228039 -0.2830118 -0.4763955  0.7582298
+# [4,]  0.2110457 -0.6006252  0.4145513 -0.8124814
+# [,1]         [,2]         [,3]          [,4]
+# [1,] 1.000000e+00 4.163336e-17 1.387779e-16 -1.665335e-16
+# [2,] 5.551115e-17 1.000000e+00 5.551115e-17  0.000000e+00
+# [3,] 0.000000e+00 1.110223e-16 1.000000e+00  0.000000e+00
+# [4,] 2.775558e-17 2.220446e-16 2.220446e-16  1.000000e+00
+# [,1] [,2] [,3] [,4]
+# [1,]    1    0    0    0
+# [2,]    0    1    0    0
+# [3,]    0    0    1    0
+# [4,]    0    0    0    1
+# [1] "correctly return identity matrix when dot product"
+# [1] "same inverse matrix returned"
